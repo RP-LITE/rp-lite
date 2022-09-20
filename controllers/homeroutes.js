f2603157a0338445f131662ea80cdab349183a85
Kurohyou <scaseydv@gmail.com>
2022-09-20 16:10:42 -0600

    Socket implementation ready to use. Modularized into sockets directory

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -3,13 +3,7 @@ const router = require('express').Router();
 const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
-    console.log('io testing');
-    const io = req.app.get('io');
     res.render('homepage');
-    io.on('testReceive',(data)=>{
-        console.log('data',data);
-        console.log('test received');
-    });
 });
 
 router.get('/login', (req, res) => {
