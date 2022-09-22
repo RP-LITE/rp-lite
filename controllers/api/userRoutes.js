ccc87986bd76582c5c31979b3f4ff8eeafbff81e
TGOLD <thomaswgold90@gmail.com>
2022-09-21 20:14:03 -0600

    login and sign up routes + frontend js, minro changes in User model, temp sign up form in views

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,7 +4,7 @@ const { User } = require("../../models");
 
 
 
-router.post("/login", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const userData = await User.create(req.body);
 
@@ -19,16 +19,58 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { user_name: req.body.user_name } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 router.get('/', async (req, res) => {
   try {
-    
+
     const userData = await User.findAll();
     res.json(userData);
-    
+
   } catch (err) {
     res.status(500).json(err);
   }
-}); 
+});
 
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,6 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeroutes = require("./homeroutes");
 router.use("/", homeroutes);
-router.get('/api',(req,res)=>{
-  console.log('hit');
-  res.send('hit it');
-})
 router.use("/api/", apiRoutes);
 
 // router.use('/api', apiRoutes)
