38ceefc07a1c7079c0f483b119630cf47bef391b
TGOLD <thomaswgold90@gmail.com>
2022-09-22 21:35:09 -0600

    routes mostly completed

--- controllers/api/profileRoutes.js
+++ controllers/api/profileRoutes.js
@@ -1,24 +1,114 @@
 const router = require("express").Router();
-const { UserObjects } = require("../../models");
+const { UserObjects, User } = require("../../models");
 
+router.get('/', async (req, res) => {
+  try {
+    const userID = req.session.user_id;
+    const creatures = await UserObjects.findAll({
+      where: {
+        user_id: userID
+      }
+    });
+    res.json(creatures);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err.message);
+  }
+});
 
-router.post('/', async (req,res) => {
-  const rockObject = UserObjects.create({
-    user_id:user.id,
-    rock_lvl: 1,
-    img:'/public/portraits/rock.png'
-  });
-  const paperObject = UserObjects.create({
-    user_id:user.id,
-    paper_lvl: 1,
-    img:'/public/portraits/rock.png'
-  });
-  const scissorObject = UserObjects.create({
-    user_id:user.id,
-    scissor_lvl: 1,
-    img:'/public/portraits/rock.png'
-  });
-  await Promise.all([rockObject,paperObject,scissorObject]);
+router.post('/:type', async (req, res) => {
+  try {
+    const type = req.params.type;
+    if (!/rock|scissor|paper/.test(type)) {
+      res.status(500).json('Invalid creature type');
+      return;
+    }
+    const newObject = await UserObjects.create({
+      user_id: req.session.user_id,
+      [`${type}_lvl`]: 1,
+      img: `/public/portratis/${type}.png`
+    });
+    res.json(newObject);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err.message);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const userID = req.session.user_id;
+    const existingObjects = await UserObjects.findAll({
+      where: {
+        user_id: userID
+      }
+    });
+    if (existingObjects.length) {
+      res.status(500).json('starter creatures already made');
+      return;
+    }
+    const newObjects = await UserObjects.bulkCreate([
+      {
+        user_id: userID,
+        rock_lvl: 1,
+        img: '/public/portraits/rock.png'
+      },
+      {
+        user_id: userID,
+        paper_lvl: 1,
+        img: '/public/portraits/rock.png'
+      },
+      {
+        user_id: userID,
+        scissor_lvl: 1,
+        img: '/public/portraits/rock.png'
+      }
+    ]);
+    res.json(newObjects);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err.message);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const userID = req.session.user_id;
+    const creature = await UserObjects.findByPk(req.params.id);
+    if (creature.user_id !== userID) {
+      res.status(500).json('Cannot access other user creature');
+      return;
+    }
+    creature.set({
+      rock_lvl: req.body.rock_lvl || creature.rock_lvl,
+      paper_lvl: req.body.paper_lvl || creature.paper_lvl,
+      scissor_lvl: req.body.scissor_lvl || creature.scissor_lvl
+    });
+    creature.save();
+    res.json(creature);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err.message);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const userID = req.session.user_id;
+    const creature = await UserObjects.findByPk(req.params.id);
+    if (!creature) {
+      res.status(200).json('Creature already deleted');
+      return
+    } else if (creature.user_id !== userID) {
+      res.status(500).json('Cannot access other user creature');
+      return;
+    }
+    creature.destroy();
+    res.json(creature);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err.message);
+  }
 })
 
 module.exports = router;
