f9ea26f9919f04eca791bf255afc5aff758eab86
TGOLD <thomaswgold90@gmail.com>
2022-09-24 14:26:33 -0600

    profile handlerbars

---
+++ public/js/profile.js
@@ -0,0 +1,15 @@
+// forEach creature render card
+// Creature Name
+// image
+// lvl
+// experience (progress bar)
+// experience/experience_threshold
+
+// forEach challenge
+// challenger user_name
+// type
+// level
+
+// challenge button
+// select your creature
+// select opponent
