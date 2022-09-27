6e78810805f7ce53244d18aa049f8973a2798a29
Alkria <adybass@gmail.com>
2022-09-26 20:50:49 -0600

    user object update

--- models/userObjects.js
+++ models/userObjects.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+const fs = require('fs/promises');
 
 class UserObjects extends Model {}
 
@@ -24,6 +25,10 @@ UserObjects.init(
       validate:{
         is: /^(?:scissor|rock|paper)$/
       }
+    },
+    name:{
+      type: DataTypes.STRING,
+      allowNull:false
     },
     rock_lvl: {
       type: DataTypes.INTEGER,
@@ -51,6 +56,25 @@ UserObjects.init(
     }
   },
   {
+    hooks:{
+      beforeCreate: async (newData) => {
+        const files = await fs.opendir(`../../public/portraits/${newData.type}`);
+        const fileArr = [];
+        console.log('======================');
+        console.log('========newData=======');
+        console.log(newdata);
+        console.log('======================');
+        for await(const dirent of dir){
+          fileArr.push(dirent.name);
+        }
+        const chosenIndex = Math.floor(Math.random() * fileArr.length);
+        newData.img = `./public/portraits/${newData.type}/${fileArr[chosenIndex]}`;
+        console.log('===================================');
+        console.log('=====================new image path',newData.img);
+        console.log('===================================');
+        return newData;
+      }
+    },
     sequelize,
     timestamps: false,
     underscored: true,
