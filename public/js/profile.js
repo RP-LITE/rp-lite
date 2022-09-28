54be990cdd21082e268e294f4f0691c84d54e47f
Alkria <adybass@gmail.com>
2022-09-27 21:35:48 -0600

    css styling for challenges cards

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,6 +3,7 @@ const $progressBars = document.querySelectorAll('.progress-bar');
 const $challengeButton = document.getElementById('start-challenge');
 const $chalCreatureSel = document.getElementById('challenge-creature');
 const $chalTargetSel = document.getElementById('challenge-target');
+const $creatureRow = document.getElementById('creature-row');
 // forEach creature render card
 // Creature Name
 // image
@@ -19,11 +20,22 @@ const $chalTargetSel = document.getElementById('challenge-target');
 /**
  * Creates the list of available users to challenge and the creatures available to choose. Also opens the challenge modal.
  */
-const openChallengeModal = async ()=>{
-  const availableUsers = await fetch('/api/challenges/search').then(data => data.text());
+const openChallengeModal = async (e)=>{
+  // get the actual button
+  const button = e.target.tagName === 'button' ?
+    e.target :
+    e.target.parentElement;
+  // If this wasn't an actual button, ignore it
+
+  if(!button?.dataset.creature) return;
+  const creature = await fetch(`/api/profile/${button.dataset.creature}`)
+    .then(data => data.json());
+  const availableUsers = await fetch('/api/challenges/search')
+    .then(data => data.text());
   console.log('availableUsers',availableUsers);
+  console.log('creature',creature);
   // const defaultUserOpt = document.createElement();
-  window.location.hash = '#challenge-modal';
+  // window.location.hash = '#challenge-modal';
 };
 // select your creature
 // select opponent
@@ -33,6 +45,6 @@ const sizeProgressBar = ($bar) => {
   const max = +$bar.dataset['aria-valuemax'] || 1;
   $bar.style.width = `${curr / max * 100}%`;
 };
-$progressBars.forEach(sizeProgressBar);
 
-$challengeButton.addEventListener('click',openChallengeModal);
+$progressBars.forEach(sizeProgressBar);
+$creatureRow.addEventListener('click',openChallengeModal);
